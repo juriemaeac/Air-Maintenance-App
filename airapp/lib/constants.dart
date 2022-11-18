a60c4fd8a381684cb57034d6aa6cd01d27083bcd
Jurie Mae Castronuevo <juriemaea@gmail.com>
2022-11-18 17:57:03 +0800

    constants

--- airapp/lib/constants.dart
+++ airapp/lib/constants.dart
@@ -6,8 +6,10 @@ class AppColors {
   static const black = Color(0xFF404040);
   static const grey = Colors.grey;
   static const greyAccent = Color(0xffF0F0F0);
+  static const greyAccentLine = Color.fromARGB(255, 218, 218, 218);
 
-  static const blueAccent = Color(0xFF36516C);
+  static const blueAccent = Color(0xFF00004d); //3E5B79
+  static const yellowAccent = Color(0xFFfcc200);
 }
 
 class AppTextStyles {
@@ -24,10 +26,45 @@ class AppTextStyles {
     fontWeight: FontWeight.w500,
     fontFamily: 'Poppins',
   );
+  static const TextStyle headings2 = TextStyle(
+    color: AppColors.black,
+    fontSize: 25,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Poppins',
+  );
 
   static const TextStyle subHeadings = TextStyle(
     color: AppColors.grey,
     fontSize: 13,
+    fontFamily: 'Poppins',
+  );
+  static const TextStyle title = TextStyle(
+    color: AppColors.black,
+    fontWeight: FontWeight.w500,
+    fontSize: 18,
+    fontFamily: 'Poppins',
+  );
+  static const TextStyle title1 = TextStyle(
+    color: AppColors.black,
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+    fontFamily: 'Poppins',
+  );
+
+  static TextStyle subtitle = GoogleFonts.quicksand(
+    color: AppColors.grey,
+    fontWeight: FontWeight.w600,
+    fontSize: 15,
+  );
+  static TextStyle subtitle1 = GoogleFonts.quicksand(
+    color: AppColors.black,
+    //fontWeight: FontWeight.w200,
+    fontSize: 15,
+  );
+  static const TextStyle subtitle2 = TextStyle(
+    color: AppColors.black,
+    fontWeight: FontWeight.w400,
+    fontSize: 15,
     fontFamily: 'Poppins',
   );
   static const TextStyle textFields = TextStyle(
